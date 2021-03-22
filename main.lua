a0e7086fbd7df3bbb3c87c9c376be1bda8f5cb63
cerebralcourt <0hhvxhv9@anonaddy.me>
2021-03-22 12:08:28 -0700

    Screens

--- main.lua
+++ main.lua
@@ -1,99 +1,17 @@
 require "lib.fun" ()
-local cartographer = require "lib.cartographer"
 local Camera = require "lib.brady"
-local anim8 = require "lib.anim8"
-local Player = require "player"
 local Music = require "music"
+local Screen = require "screen"
 
 math.randomseed(os.time())
 
-local world,
-      tilemap,
-      entry,
-      exit,
-      player,
-      cam,
-      music,
-      doorimage,
-      entryanim,
-      exitanim,
-      nextFrame
+local world, cam, music, screens, screen
 
-function createFixture(col)
-  local x = col.x
-  local y = col.y
-
-  if col.height == 12 then
-    y = y - 10
-  end
-
-  if col.height == 4 then
-    y = y - 14
-    x = x - 8
-  end
-
-  if col.width > col.height then
-    if col.width == 64 then
-      x = x + 16
-    else
-      x = x + col.width / 2
-    end
-  elseif col.height > col.width then
-    if col.height == 64 then
-      y = y + 16
-    else
-      y = y + col.height / 2
-    end
-  end
-
-  local body = love.physics.newBody(world, x, y)
-  local shape = love.physics.newRectangleShape(col.width, col.height)
-  local fixture = love.physics.newFixture(body, shape)
-  fixture:setFriction(0)
-  return fixture
-end
-
-function beginContact(a, b, coll)
-  local fixture = nil
-  local x1, y1, x2, y2
-  if player.fixture == a then
-    fixture = b
-  elseif player.fixture == b then
-    fixture = a
-  end
-
-  if fixture then
-    if fixture:getCategory() == 2 then
-      local y1 = player.body:getY()
-      local y2 = fixture:getBody():getY()
-
-      if y1 < y2 then
-        nextFrame = function() fixture:setSensor(false) end
-      else
-        fixture:setSensor(true)
-      end
-    end
-  end
-end
-
-function endContact(a, b, coll)
-  
-end
-
-function preSolve(a, b, coll)
-  
-end
-
-function postSolve(a, b, coll, normalimpulse, tangentimpulse)
-  if player.jumping and (player.fixture == a or player.fixture == b) then
-    local nx, ny = coll:getNormal()
-
-    if nx == 0 and ny == -1 and player.anim ~= player.anims.exit then
-      player.jumping = false
-      player.anim = player.anims.ground
-      player.groundtimeout = 0.2
-    end
-  end
+function changeScreen(nextScreen)
+  world:destroy()
+  world = love.physics.newWorld(0, 9.81 * 32)
+  screen = nextScreen
+  screen:init(world, screens, changeScreen)
 end
 
 function love.load()
@@ -103,78 +21,32 @@ function love.load()
   love.graphics.setLineWidth(3)
 
   world = love.physics.newWorld(0, 9.81 * 32)
-  tilemap = cartographer.load "levels/1_1.lua"
-
-  local function objects(name)
-    return totable(filter(function(obj) return obj.name == name end, tilemap.layers.Objects.objects))
-  end
-
-  entry = objects("Entry")[1]
-  exit = objects("Exit")[1]
-
-  for i, col in ipairs(tilemap.layers.Collisions.objects) do
-    createFixture(col)
-  end
-
-  for i, col in ipairs(tilemap.layers.Sensors.objects) do
-    local fixture = createFixture(col)
-    fixture:setSensor(true)
-    fixture:setCategory(col.type or 1)
-  end
-
-  player = Player(world, entry, exit)
   cam = Camera(32 * 8, 32 * 6, { resizable = true, maintainAspectRatio = true })
   music = Music()
-
-  doorimage = love.graphics.newImage("assets/sprites/door.png")
-  local doorg = anim8.newGrid(46, 56, doorimage:getWidth(), doorimage:getHeight())
-  exitanim = anim8.newAnimation(doorg("1-3", 1), 0.1, "pauseAtEnd")
-  entryanim = anim8.newAnimation(doorg("1-3", 2), { 0.6, 0.1, 0.1 }, function() entryanim = nil end)
-
-  world:setCallbacks(beginContact, endContact, preSolve, postSolve)
+  screens = {
+    levels = {
+      {
+        Screen("1_1"),
+      },
+    },
+  }
+  screen = screens.levels[1][1]
+  screen:init(world, screens, changeScreen)
 end
 
 function love.update(dt)
-  if nextFrame then
-    nextFrame()
-    nextFrame = nil
-  end
-
   music:update(dt)
   world:update(dt)
-  tilemap:update(dt)
+  screen:update(dt)
 
-  if entryanim then
-    entryanim:update(dt)
-  end
-
-  player:update(dt)
-
-  if player.anim == player.anims.exit then
-    exitanim:update(dt)
-  end
-
-  cam.translationX = player.body:getX() + player.width / 2
-  cam.translationY = player.body:getY() + player.height / 2
+  cam.translationX = screen:getX()
+  cam.translationY = screen:getY()
   cam:update()
 end
 
 function love.draw()
   love.graphics.clear(63 / 255, 56 / 255, 81 / 255)
-
   cam:push()
-
-  tilemap:draw()
-
-  if entryanim then
-    entryanim:draw(doorimage, entry.x + 1, entry.y)
-  end
-
-  if player.anim == player.anims.exit then
-    exitanim:draw(doorimage, exit.x, exit.y)
-  end
-
-  player:draw()
-
+  screen:draw()
   cam:pop()
 end
---
+++ screen.lua
@@ -0,0 +1,170 @@
+local cartographer = require "lib.cartographer"
+local anim8 = require "lib.anim8"
+local Player = require "player"
+
+local tilemap,
+      entry,
+      exit,
+      player,
+      doorimage,
+      entryanim,
+      exitanim,
+      nextFrame
+
+local function createFixture(world, col)
+  local x = col.x
+  local y = col.y
+
+  if col.height == 12 then
+    y = y - 10
+  end
+
+  if col.height == 4 then
+    y = y - 14
+    x = x - 8
+  end
+
+  if col.width > col.height then
+    if col.width == 64 then
+      x = x + 16
+    else
+      x = x + col.width / 2
+    end
+  elseif col.height > col.width then
+    if col.height == 64 then
+      y = y + 16
+    else
+      y = y + col.height / 2
+    end
+  end
+
+  local body = love.physics.newBody(world, x, y)
+  local shape = love.physics.newRectangleShape(col.width, col.height)
+  local fixture = love.physics.newFixture(body, shape)
+  fixture:setFriction(0)
+  return fixture
+end
+
+local function beginContact(a, b, coll)
+  local fixture = nil
+  local x1, y1, x2, y2
+  if player.fixture == a then
+    fixture = b
+  elseif player.fixture == b then
+    fixture = a
+  end
+
+  if fixture then
+    if fixture:getCategory() == 2 then
+      local y1 = player.body:getY()
+      local y2 = fixture:getBody():getY()
+
+      if y1 < y2 then
+        nextFrame = function() fixture:setSensor(false) end
+      else
+        fixture:setSensor(true)
+      end
+    end
+  end
+end
+
+local function endContact(a, b, coll)
+  
+end
+
+local function preSolve(a, b, coll)
+  
+end
+
+local function postSolve(a, b, coll, normalimpulse, tangentimpulse)
+  if player.jumping and (player.fixture == a or player.fixture == b) then
+    local nx, ny = coll:getNormal()
+
+    if nx == 0 and ny == -1 and player.anim ~= player.anims.exit then
+      player.jumping = false
+      player.anim = player.anims.ground
+      player.groundtimeout = 0.2
+    end
+  end
+end
+
+return function(name)
+  local screen = {}
+
+  function screen:init(world, screens, changeScreen)
+  	tilemap = cartographer.load("levels/" .. name .. ".lua")
+
+    local function objects(name)
+      return totable(filter(function(obj) return obj.name == name end, tilemap.layers.Objects.objects))
+    end
+
+    entry = objects("Entry")[1]
+    exit = objects("Exit")[1]
+
+    for i, col in ipairs(tilemap.layers.Collisions.objects) do
+      createFixture(world, col)
+    end
+
+    for i, col in ipairs(tilemap.layers.Sensors.objects) do
+      local fixture = createFixture(world, col)
+      fixture:setSensor(true)
+      fixture:setCategory(col.type or 1)
+    end
+
+    player = Player(world, entry, exit)
+
+    doorimage = love.graphics.newImage("assets/sprites/door.png")
+    local doorg = anim8.newGrid(46, 56, doorimage:getWidth(), doorimage:getHeight())
+    exitanim = anim8.newAnimation(doorg("1-3", 1), { 0.1, 0.1, 0.6 },
+      function()
+        exitanim:pauseAtEnd()
+        changeScreen(screens.levels[1][1])
+      end)
+    entryanim = anim8.newAnimation(doorg("1-3", 2), { 0.6, 0.1, 0.1 }, function() entryanim = nil end)
+
+    world:setCallbacks(beginContact, endContact, preSolve, postSolve)
+  end
+
+  function screen:update(dt)
+    if nextFrame then
+      nextFrame()
+      nextFrame = nil
+    end
+
+    tilemap:update(dt)
+
+    if entryanim then
+      entryanim:update(dt)
+    end
+
+    player:update(dt)
+
+    if player.anim == player.anims.exit then
+      exitanim:update(dt)
+    end
+  end
+
+  function screen:draw()
+    tilemap:draw()
+
+    if entryanim then
+      entryanim:draw(doorimage, entry.x + 1, entry.y)
+    end
+
+    if player.anim == player.anims.exit then
+      exitanim:draw(doorimage, exit.x, exit.y)
+    end
+
+    player:draw()
+  end
+
+  function screen:getX()
+    return player.body:getX() + player.width / 2
+  end
+
+  function screen:getY()
+    return player.body:getY() + player.height / 2
+  end
+
+  return screen
+end
